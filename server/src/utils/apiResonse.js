433188d5ca993944b7a45d566a0a04e04b8ec6bf
suthakaranburaj <suthakaranburaj@gmail.com>
2025-02-15 11:33:57 +0530

    initializing project

---
+++ server/src/utils/apiResonse.js
@@ -0,0 +1,11 @@
+export default sendResponse = function (res, status, data, message, statusCode = 200, apiVersion = null) {
+    let obj = {
+        status,
+        data,
+        message,
+        // statusCode,
+        apiVersion: apiVersion || 'No Version'
+    };
+
+    return res.status(statusCode).json(obj);
+};
