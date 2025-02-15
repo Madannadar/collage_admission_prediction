626d2b6816419526fc499b3ad38820af7a2faf1d
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-02-15 13:59:52 +0530

    1.59

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import DepartmentForm from "./Pages/DepartmentForm" // Adjust the import path as needed
+import { Routes, Route } from "react-router-dom";
+import DepartmentForm from "./Pages/DepartmentForm";
+import FacultyDetails from "./Pages/FacultyDetails";
+import Head from "./Pages/Head";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-[var(--bg-color)] p-6">
-      <DepartmentForm />
+      {/* <Routes>
+        <Route path="/" element={<Head/>} />
+        <Route path="/DepartmentForm" element={<DepartmentForm />} />
+        <Route path="/FacultyDetails" element={<FacultyDetails />} />
+      </Routes> */}
     </div>
   );
 };
