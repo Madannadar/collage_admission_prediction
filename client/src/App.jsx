7e8f8d457c64cc9b24adc555a80db15ed6919780
sgokul1500 <sgokul1500@gmail>
2025-02-15 13:13:50 +0530

    department form done 13.13

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DepartmentForm from "./Pages/DepartmentForm" // Adjust the import path as needed
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-[var(--bg-color)] p-6">
+      <DepartmentForm />
+    </div>
+  );
+};
+
+export default App;
