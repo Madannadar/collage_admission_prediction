a3a679ca9f092b4183dc6db2c09663b3a10d3074
suthakaranburaj <suthakaranburaj@gmail.com>
2025-02-15 13:41:18 +0530

    1.41

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <p className='bg-red-500'>Hello</p>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
